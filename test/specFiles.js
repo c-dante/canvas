5655c49004b6c3ba0c81ee154495e866568b1986
dante <c.dante.federici@gmail.com>
2019-06-12 01:12:12 -0400

    Moved app to src.

--- test/specFiles.js
+++
@@ -1,1 +0,0 @@
-'use strict'; var out = ["test/main/example.spec.js"]; if (typeof define==='function'){define(function(require){return out;});}if (typeof module!=='undefined'){module.exports = out;}
